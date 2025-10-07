43ae129c0a8e6afce57d448c7242d97aa48d3438
Mahyar Mirrashed <mah.mirr@gmail.com>
2025-10-07 00:54:04 -0500

    routes/block-demo.svelte: add component to showcase blocks inside

---
+++ src/routes/block-demo.svelte
@@ -0,0 +1,25 @@
+<script lang="ts" module>
+  type Props = {
+    name: Snippet;
+    description: Snippet;
+  };
+</script>
+
+<script lang="ts">
+  import type { WithChildren } from "bits-ui";
+  import type { Snippet } from "svelte";
+
+  let { name, description, children }: WithChildren<Props> = $props();
+</script>
+
+<div class="relative flex flex-col gap-4 rounded-lg border p-4">
+  <div class="flex flex-col gap-1 pt-1 sm:pl-3">
+    <h2 class="font-medium opacity-80">{@render name()}</h2>
+    <h2 class="w-lg text-sm text-muted-foreground">
+      {@render description()}
+    </h2>
+  </div>
+  <div class="relative flex items-center justify-center p-4">
+    {@render children?.()}
+  </div>
+</div>
